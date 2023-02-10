f99b01beefa5c81178f1d5fb2cebd3f5265a3098
Rahul Kumar <r1303yadav@gmail.com>
2023-02-10 18:08:54 +0530

    initial commit

---
+++ Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeBook
+{
+    class Employee
+    {
+        private int id;
+        private string name;
+        private string department;
+
+        public delegate void MethodCalledEventHandler(string message);
+        public event MethodCalledEventHandler MethodCalled;
+
+        //Creating constructor of Employee class 
+        public Employee(int empId, string empName, string empDep)
+        {
+            id = empId;
+            name = empName;
+            department = empDep;
+        }
+        public int GetId()
+        {
+            OnMethodCalled("GetId() method called");
+            return id;
+        }
+        public string GetName()
+        {
+            OnMethodCalled("GetName() method called");
+            return name;
+
+        }
+        public string GetDepartmentName()
+        {
+            OnMethodCalled("GetDepartmentName() method called");
+            return department;
+        }
+
+
+
+        // Updating employee id using Update method
+        public void Update(int empId)
+        {
+            OnMethodCalled("Your Employee id is updated...");
+            id = empId;
+        }
+
+        // Updating employee Name using Update method
+        public void Update(string empName)
+        {
+            OnMethodCalled("Your Employee Name is updated...");
+            name = empName;
+        }
+
+        // Updating employee id using Update method
+        public void Update(int empId, string empDepartment)
+        {
+            OnMethodCalled("Your Employee id is updated...");
+            id = empId;
+            department = empDepartment;
+        }
+        protected void OnMethodCalled(string message)
+        {
+            MethodCalled?.Invoke(message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace EmployeeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your details........");
+            Console.Write("ID no: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Department name: ");
+            string depName = Console.ReadLine();
+
+            Employee emp = new Employee(id, name, depName);
+
+            emp.MethodCalled += Employee_MethodCalled;
+
+            Console.WriteLine();
+            //method calling.
+            string empName = emp.GetName();
+            int empID = emp.GetId();
+            string empDep = emp.GetDepartmentName();
+
+
+            //printing employee details
+            Console.WriteLine();
+            Console.WriteLine("Here is your details.......");
+            Console.WriteLine("Employee ID :" + empID);
+            Console.WriteLine("Employee Name: " + empName);
+            Console.WriteLine("Department Name :" + empDep);
+            Console.WriteLine();
+
+
+            while (true)
+            {
+                Console.WriteLine("Do you want to update your details(y/n)");
+                string userInput = Console.ReadLine();
+                if (userInput.Equals("y") || userInput.Equals("Y"))
+                {
+                    Console.WriteLine("What you want to update: 1. ID, 2. Name, 3. Department");
+
+                    int option = Convert.ToInt32(Console.ReadLine());
+
+                    switch (option)
+                    {
+                        case 1:
+                            Console.Write("Enter your new ID no. ");
+                            int newId = Convert.ToInt32(Console.ReadLine());
+                            emp.Update(newId);                                   //using method overloading 
+                            break;
+                        case 2:
+                            Console.Write("Enter your new Name: ");
+                            String newName = Console.ReadLine();
+                            emp.Update(newName);                                 //using method overloading 
+                            break;
+                        case 3:
+                            Console.Write("Enter your new ID no.: ");
+                            int newIdD = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter your new Department name: ");
+                            string newDep = Console.ReadLine();
+                            emp.Update(newIdD, newDep);                      //using method overloading 
+                            break;
+                        default:
+                            Console.WriteLine("You entered invalid option.");
+                            break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+        }
+        private static void Employee_MethodCalled(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
